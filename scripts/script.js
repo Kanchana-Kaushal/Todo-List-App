4242f78a3a349f938e92dfabf4179cef3b672c6d
Kanchana K. <162160183+Kanchana-Kaushal@users.noreply.github.com>
2025-01-07 20:58:49 +0530

    Fully functional base version
    added features which are set data to save in the local storage, show completed tasks, delete tasks and some event listeners like keydown.

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,70 @@
-const todoArray = [];
+const todoArray = JSON.parse(localStorage.getItem("storedTodo"));
+displayTodo();
+
+//this part prevents the default form submission and page refreshing.
+formElem = document.querySelector("form");
+formElem.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission
+});
+
+formElem.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        inputTodo();
+    }
+});
 
 function inputTodo() {
     let inputValue = document.querySelector("#input-task").value;
-    todoArray.push(inputValue);
-    document.querySelector("#input-task").value = "";
-    displayTodo();
+
+    const todoObject = {
+        name: inputValue,
+        completed: false,
+    };
+
+    if (inputValue) {
+        todoArray.push(todoObject);
+        document.querySelector("#input-task").value = "";
+        displayTodo();
+        localStorage.setItem("storedTodo", JSON.stringify(todoArray));
+    }
 }
 
 function displayTodo() {
     let HTML = "";
     for (let i = 0; i < todoArray.length; i++) {
-        HTML += `<li class="tasks">
+        if (todoArray[i].completed === false) {
+            HTML += `<li class="tasks">
             <input type="checkbox" id="check-done-${i}" />
     
-            <label for="check-done-${i}" class="custom-checkbox">
+            <label for="check-done-${i}" class="custom-checkbox" onclick="markComplete(${i});">
                 <i class="fa-solid fa-check"></i>
             </label>
     
-            <label for="check-done-${i}" class="task-description"
-                >${todoArray[i]}
+            <label for="check-done-${i}" class="task-description"  onclick="markComplete(${i});"
+                >${todoArray[i].name}
             </label>
     
             <button class="del-btn" onclick = "deleteTodo(${i});">
                 <i class="fa-solid fa-trash"></i>
             </button>
             </li>`;
+        } else if (todoArray[i].completed === true) {
+            HTML += `<li class="tasks">
+            <input type="checkbox" id="check-done-${i}" class="new-class" checked/>
+    
+            <label for="check-done-${i}" class="custom-checkbox" onclick="markComplete(${i});">
+                <i class="fa-solid fa-check"></i>
+            </label>
+    
+            <label for="check-done-${i}" class="task-description"  onclick="markComplete(${i});"
+                >${todoArray[i].name}
+            </label>
+    
+            <button class="del-btn" onclick = "deleteTodo(${i});">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+            </li>`;
+        }
     }
 
     document.querySelector(".todoList").innerHTML = HTML;
@@ -33,4 +73,22 @@ function displayTodo() {
 function deleteTodo(i) {
     todoArray.splice(i, 1);
     displayTodo();
+    localStorage.setItem("storedTodo", JSON.stringify(todoArray));
+}
+
+function markComplete(i) {
+    let checkbox = document.querySelector(`#check-done-${i}`);
+    //this part works when the checkbox is unchecked
+    if (checkbox.checked) {
+        checkbox.classList.remove("new-class");
+        todoArray[i].completed = false;
+    }
+
+    //this part works when checkbox is checked
+    else if (!checkbox.checked) {
+        checkbox.classList.add("new-class");
+        todoArray[i].completed = true;
+    }
+
+    localStorage.setItem("storedTodo", JSON.stringify(todoArray));
 }
